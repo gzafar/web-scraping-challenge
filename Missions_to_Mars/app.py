40b212233f6dc4ee018d149b7fe2db85a330404d
unknown <grazielzafar@gmail.com>
2022-01-08 23:19:56 -0600

    updated app.py and scrape_mars.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,3 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_pymongo import pymongo
+import scrape_mars
 
 app = Flask(__name__)
+
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+mongo = pymongo(app, uri="mongodb://localhost:27017/mars_db")
+
+@app.route("/")
+def index():
+    mars_dict = mongo.db.mars_dict.find_one()
+    return render_template("index.html", mars_dict=mars_dict)
+
+@app.route("/scrape")
+def scrape():
+    mars_dict = mongo.db.mars_dict
+    mars_data = scrape_mars.scrape()
+    mars_dict.update({}, mars_data, upsert=True)
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -2,20 +2,99 @@ from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import pymongo
 import requests
+from flask import Flask, render_template
+import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
 
-def init_browser():
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
 
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 
+def scrape():
+    browser=init_browser()
+    mars_dict = {}
+
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+    # NASA Mars News
+    news_title = soup.find_all('div', class_='content_title')[0].text
+    news_p = soup.find_all('div', class_='article_teaser_body')[0].text
+
+    # JPL Mars Space Images - Featured
+    space_images_url = 'https://spaceimages-mars.com/'
+    browser.visit(space_images_url)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+    image_url = soup.find('img', class_='headerimage fade-in')['src']
+
+    featured_img_url = space_images_url+image_url
+
+    # Mars Facts
+    mars_url = 'https://galaxyfacts-mars.com/'
+    browser.visit(mars_url)
+
+    table = pd.read_html(mars_url)
+    df = table[0]
+
+    html_table = df.to_html('table.html')
+
+    # Mars Hemispheres
+    mars_hemispheres_url = 'https://marshemispheres.com/'
+    browser.visit(mars_hemispheres_url)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+    hemispheres = soup.find('div', class_='collapsible results')
+
+    mars_hemispheres = hemispheres.find_all('div', class_='item')
+    mars_hemispheres = []
+
+    for hemisphere in mars_hemispheres:
+        img_desc = hemisphere.find('div', class_='description')
+        title = hemisphere.h3.text
+
+        hemisphere_url = img_desc.a['href']
+        
+        browser.visit(mars_hemispheres_url)
+        html = browser.html
+        soup = bs(html, 'html.parser')
+        img_src = soup.find('img', class_='thumb')['src']
+        
+        if(title and img_src):
+            print(title)
+            print(mars_hemispheres_url + img_src)
+            
+        hemisphere_dict = {
+            'title': title,
+            'img_src': img_src
+        }
+        
+    mars_hemispheres.append(hemisphere_dict)
 
 
 
+    mars_dict = {
+    'news_title': news_title,
+    'news_p': news_p,
+    'featured_img_url': featured_img_url,
+    'html_table': html_table,
+    'mars_hemispheres': mars_hemispheres
+    }   
 
+    mars_dict
 
+    browser.quit()
 
+    return mars_dict
 
 
 
